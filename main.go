167e370b1b7a5a2a431a4444afeaa362a6a154bb
whatsyourask <lexeymartynov@gmail.com>
2024-04-05 23:30:10 +0300

    Refactoring - from global variables to structure

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"divine-dragon/remote_exploit"
+	"divine-dragon/remote_enum"
 )
 
 func main() {
-	remote_exploit.SetupASREPRoastingModule("htb.local", "10.129.95.210", false, false, false, "", "remote_exploit/names.txt", "", 10, 0)
-	remote_exploit.ASREPRoasting()
+	// asrep_module := remote_exploit.SetupModule("htb.local", "10.129.40.155", false, false, false, "", "remote_exploit/names.txt", "", 10, 0)
+	// asrep_module.Run()
+
+	kerb_enum_module := remote_enum.SetupModule("htb.local", "10.129.40.155", false, false, false, "remote_exploit/names.txt", "", 80, 0)
+	kerb_enum_module.Run()
 }
