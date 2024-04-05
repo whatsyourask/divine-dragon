73c0cc95352683b0745d53f895743b785f02f9c1
whatsyourask <lexeymartynov@gmail.com>
2024-04-06 00:04:33 +0300

    Again a little bit of refactoring - Changed constructor function name to New<NameOfTheModule>()

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ func main() {
 	// asrep_module := remote_exploit.SetupModule("htb.local", "10.129.40.155", false, false, false, "", "remote_exploit/names.txt", "", 10, 0)
 	// asrep_module.Run()
 
-	kerb_enum_module := remote_enum.SetupModule("htb.local", "10.129.40.155", false, false, false, "remote_exploit/names.txt", "", 80, 0)
+	kerb_enum_module := remote_enum.NewKerberosEnumUsersModule("htb.local", "10.129.40.155", false, false, false, "remote_exploit/names.txt", "", 80, 0)
 	kerb_enum_module.Run()
 }
