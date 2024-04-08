da145cd8b51fea1d1b29f0c61f32cd179fae7a65
whatsyourask <lexeymartynov@gmail.com>
2024-04-08 23:19:02 +0300

    Moved ConstructBaseDN from transport/ldap module

--- util/ldap.go
+++ util/ldap.go
@@ -1,10 +1,22 @@
 package util
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/go-ldap/ldap"
 )
+
+func ConstructBaseDN(domain string) (string, error) {
+	domainName := strings.Split(domain, ".")
+	if len(domainName) != 2 {
+		return "", errors.New("invalid domain name")
+	}
+	tld := domainName[1]
+	sld := domainName[0]
+	return fmt.Sprintf("dc=%s,dc=%s", sld, tld), nil
+}
 
 func ConstructLDAPFilter(template string, values []any) string {
 	filter := fmt.Sprintf(template, values...)
