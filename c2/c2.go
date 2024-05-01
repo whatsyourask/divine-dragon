0511c3934a9967be85c2fde4bdc35aec7aabbdf3
whatsyourask <lexeymartynov@gmail.com>
2024-05-01 22:46:49 +0300

    Added a few logging messages and also added an Operator's password generation

--- c2/c2.go
+++ c2/c2.go
@@ -18,7 +18,10 @@ func NewC2Module(localHostOpt, localPortOpt string) *C2Module {
 		localPort: localPortOpt,
 	}
 	c2m.logger = util.C2Logger(false, "")
-	c2, err := transport.NewC2Server(localHostOpt, localPortOpt)
+	c2m.logger.Log.Info("Initializing a C2 server...")
+	password := util.RandString(24)
+	c2m.logger.Log.Infof("Operator account has the following password - %s\n", password)
+	c2, err := transport.NewC2Server(localHostOpt, localPortOpt, password)
 	if err != nil {
 		c2m.logger.Log.Error(err)
 		return nil
@@ -28,5 +31,18 @@ func NewC2Module(localHostOpt, localPortOpt string) *C2Module {
 }
 
 func (c2m *C2Module) Run() {
-	go c2m.c2s.Run()
+	c2m.logger.Log.Infof("A new C2 server started on %s:%s", c2m.localHost, c2m.localPort)
+	go c2m.protect(c2m.c2s.Run)
+}
+
+func (c2m *C2Module) protect(f func() error) {
+	defer func() {
+		if err := recover(); err != nil {
+			c2m.logger.Log.Noticef("Recovered C2 server: %v", err)
+		}
+	}()
+	err := f()
+	if err != nil {
+		c2m.logger.Log.Error(err)
+	}
 }
